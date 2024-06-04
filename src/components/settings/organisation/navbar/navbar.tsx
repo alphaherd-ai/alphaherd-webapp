84e46d6329a510dbd03e76ee5879c9fec697d26f
Kunalpal216 <kunalpal2115@gmail.com>
2024-06-04 15:58:38 +0530

    added invoice pdf route

--- src/components/settings/organisation/navbar/navbar.tsx
+++ src/components/settings/organisation/navbar/navbar.tsx
@@ -11,9 +11,9 @@ import Popup from "@/components/auth/addUsersPopup"
 
 const OrganisationNavbar = () => {
     const currentRoute = usePathname();
-    
+
     const [showPopup, setShowPopup] = React.useState(false);
-const togglePopup = () => {
+    const togglePopup = () => {
         setShowPopup(!showPopup);
     }
 
@@ -24,34 +24,34 @@ const togglePopup = () => {
 
             <div className="w-full p-6 bg-white rounded-tl-[10px] rounded-tr-[10px] border border-neutral-400 justify-between items-center gap-6 flex">
                 <div className="flex">
-                <Link className='no-underline ' href='/settings/organisation/myorg'>
-                    <div className={currentRoute.startsWith("/settings/organisation/myorg")
+                    <Link className='no-underline ' href='/settings/organisation/myorg'>
+                        <div className={currentRoute.startsWith("/settings/organisation/myorg")
                             ? "px-2 py-1 bg-zinc-900 rounded-tl-[5px] rounded-bl-[5px] border border-white justify-start items-center gap-1 flex text-white text-sm font-bold "
                             : " px-2 py-1 bg-gray-100 rounded-tl-[5px] rounded-bl-[5px] border border-neutral-400 justify-start items-center gap-1 flex text-neutral-400 text-sm font-bold "} >
-                        Clinic details
-                    </div>
+                            Clinic details
+                        </div>
                     </Link>
                     <Link className='no-underline ' href='/settings/organisation/usersandrole'>
-                   <div className={currentRoute.startsWith("/settings/organisation/usersandrole")
+                        <div className={currentRoute.startsWith("/settings/organisation/usersandrole")
                             ? "px-2 py-1 bg-zinc-900 rounded-tr-[5px] rounded-br-[5px] border border-white justify-start items-center gap-1 flex text-white text-sm font-bold "
                             : " px-2 py-1 bg-gray-100 rounded-tl-[5px] rounded-bl-[5px] border border-neutral-400 justify-start items-center gap-1 flex text-neutral-400 text-sm font-bold "}>
-                        Users and Roles
-                    </div>
+                            Users and Roles
+                        </div>
                     </Link>
 
                 </div>
                 {/* <div className='flex items-center h-9 px-4 py-2.5 bg-black justify-between rounded-lg '> */}
 
-                    {/* <Popover placement="bottom-end" showArrow offset={10}> */}
-                        {/* <PopoverTrigger> */}
-                            <Button 
-                                variant="solid"
-                                className="capitalize border-none bg-black py-2.5 text-white rounded-lg flex gap-2 justify-center items-center hover:cursor-pointer" onClick={togglePopup}>
-                                <div className='flex'><Image src={addUserIcon} alt='addUserIcon' className='w-6 h-6 ' /></div>
-                                 <span>Add User</span>
-                            </Button>
-                        {/* </PopoverTrigger> */}
-                        {/* <PopoverContent className="p-5 bg-black text-white flex flex-row items-start rounded-lg border-2 ,t-3 mt-2.5">
+                {/* <Popover placement="bottom-end" showArrow offset={10}> */}
+                {/* <PopoverTrigger> */}
+                <Button
+                    variant="solid"
+                    className="capitalize border-none bg-black py-2.5 text-white rounded-lg flex gap-2 justify-center items-center hover:cursor-pointer" onClick={togglePopup}>
+                    <div className='flex'><Image src={addUserIcon} alt='addUserIcon' className='w-6 h-6 ' /></div>
+                    <span>Add User</span>
+                </Button>
+                {/* </PopoverTrigger> */}
+                {/* <PopoverContent className="p-5 bg-black text-white flex flex-row items-start rounded-lg border-2 ,t-3 mt-2.5">
 
                             <div className="flex flex-col ">
 
@@ -68,12 +68,12 @@ const togglePopup = () => {
 
 
                         </PopoverContent> */}
-                    {/* </Popover> */}
+                {/* </Popover> */}
 
-                    {showPopup && <Popup onClose={togglePopup} />}
+                {showPopup && <Popup onClose={togglePopup} />}
 
 
-                </div>
+            </div>
             {/* </div > */}
 
 
