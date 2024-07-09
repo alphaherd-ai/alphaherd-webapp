302a58887aba7d1ac5d72fbbc4301b8aa039a376
Siddhant <pokefansiddhant@gmail.com>
2024-07-09 15:25:39 +0530

    some changes

--- src/lib/bull.ts
+++ src/lib/bull.ts
@@ -1,7 +1,24 @@
-// lib/bull.ts
-import  Queue  from 'bull';
-import prismaClient from '../../prisma';
-import { calculateNextOccurrence } from '../utils/calculateNextOccurrence'; 
+const redis = require('redis');
+const Queue = require('bull');
+const prismaClient = require('../../prisma');
+const { calculateNextOccurrence } = require('../utils/calculateNextOccurrence');
+
+console.log("we're here");
+
+
+const redisClient = redis.createClient({
+  host: 'localhost',
+  port: 6379,
+});
+
+redisClient.on('error', (err: any) => {
+  console.log('Redis error:', err);
+});
+
+redisClient.on('connect', () => {
+  console.log('Redis connected');
+  redisClient.quit(); // Close the connection after verification
+});
 
 const recurringExpensesQueue = new Queue('recurring-expenses', {
   redis: {
@@ -10,7 +27,9 @@ const recurringExpensesQueue = new Queue('recurring-expenses', {
   },
 });
 
-recurringExpensesQueue.process(async (job:any) => {
+console.log('we reached here');
+
+recurringExpensesQueue.process(async (job: { data: { expenseId: any; }; }) => {
   const { expenseId } = job.data;
   const expenseRecord = await prismaClient.expenses.findUnique({ where: { id: expenseId } });
 
@@ -24,17 +43,17 @@ recurringExpensesQueue.process(async (job:any) => {
     throw new Error('Expense not found');
   }
 
-  const nextDate = calculateNextOccurrence(expense.recurringStartedOn!, expense.recurringRepeatType!);
+  const nextDate = calculateNextOccurrence(expense.recurringStartedOn, expense.recurringRepeatType);
 
-  if (nextDate <= new Date(expense.recurringEndson!)) {
-    const newExpense=await prismaClient.expenses.create({
+  if (nextDate <= new Date(expense.recurringEndson)) {
+    const newExpense = await prismaClient.expenses.create({
       data: {
         ...expense,
         date: new Date(),
       },
     });
 
-    recurringExpensesQueue.add({ expenseId: newExpense.id  }, { delay: nextDate.getTime() - Date.now() });
+    recurringExpensesQueue.add({ expenseId: newExpense.id }, { delay: nextDate.getTime() - Date.now() });
   }
 });
 
