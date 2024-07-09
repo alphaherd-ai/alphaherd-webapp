11c2ff5453189f3b63d5460e5f9ecb6d14990b02
Siddhant <103687316+Sid2728@users.noreply.github.com>
2024-07-09 16:11:02 +0530

    Update bull.ts

--- src/lib/bull.ts
+++ src/lib/bull.ts
@@ -1,24 +1,7 @@
+// lib/bull.ts
 import  Queue  from 'bull';
-import { createClient } from 'redis';
 import prismaClient from '../../prisma';
-
-import { calculateNextOccurrence } from '../utils/calculateNextOccurrence';
-
-console.log("we're here");
-
-// Create a Redis client to verify connection
-const redisClient = createClient({
-  url: 'redis://localhost:6379'
-});
-
-redisClient.on('error', (err: any) => {
-  console.log('Redis error:', err);
-});
-
-redisClient.on('connect', () => {
-  console.log('Redis connected');
-  redisClient.quit(); // Close the connection after verification
-});
+import { calculateNextOccurrence } from '../utils/calculateNextOccurrence'; 
 
 const recurringExpensesQueue = new Queue('recurring-expenses', {
   redis: {
@@ -27,9 +10,7 @@ const recurringExpensesQueue = new Queue('recurring-expenses', {
   },
 });
 
-console.log('we reached here');
-
-recurringExpensesQueue.process(async (job) => {
+recurringExpensesQueue.process(async (job:any) => {
   const { expenseId } = job.data;
   const expenseRecord = await prismaClient.expenses.findUnique({ where: { id: expenseId } });
 
@@ -46,14 +27,14 @@ recurringExpensesQueue.process(async (job) => {
   const nextDate = calculateNextOccurrence(expense.recurringStartedOn!, expense.recurringRepeatType!);
 
   if (nextDate <= new Date(expense.recurringEndson!)) {
-    const newExpense = await prismaClient.expenses.create({
+    const newExpense=await prismaClient.expenses.create({
       data: {
         ...expense,
         date: new Date(),
       },
     });
 
-    recurringExpensesQueue.add({ expenseId: newExpense.id }, { delay: nextDate.getTime() - Date.now() });
+    recurringExpensesQueue.add({ expenseId: newExpense.id  }, { delay: nextDate.getTime() - Date.now() });
   }
 });
 
