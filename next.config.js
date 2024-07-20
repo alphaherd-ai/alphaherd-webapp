ecd1308c07bb67f5b4aa82566feb808efcdde429
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2024-07-21 03:31:43 +0530

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH,
-  env: {
-     NEXTAUTH_URL: process.env.NEXTAUTH_URL
-  },
-  reactStrictMode: false,
+  
   images: {
         domains: ['res.cloudinary.com'],
       },
