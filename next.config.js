e890753bcc0c96536248a2721c48e0583beca177
aditya_dadhich <128921226+TheAdich@users.noreply.github.com>
2025-01-17 21:30:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
-  }
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mp3$/, // Match .mp3 files
+      type: 'asset/resource', // Handle as a resource
+      generator: {
+        filename: 'static/media/[name].[hash][ext]', // Output path for processed files
+      },
+    });
+
+    return config;
+  },
 };
+
 module.exports = nextConfig;
