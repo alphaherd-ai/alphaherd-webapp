14225165e8031090fe7791e32a90168072b62954
guptashivam-9370 <shivamg9425@gmail.com>
2025-01-18 19:13:14 +0530

    fixed stay logged in

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,10 +4,11 @@ import store from "@/lib/store"; // Import your store
 
 export async function POST() {
   cookies().set("session", "", { expires: new Date(0) });
+  cookies().set("sessionid", "", { expires: new Date(0) });
   if (typeof window !== "undefined") {
     localStorage.clear();
   }
-  console.log("Session cookie cleared");
+  // console.log("Session cookie cleared");
 
   // Dispatch the USER_LOGOUT action to reset the state
   store.dispatch({ type: 'USER_LOGOUT' });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,16 @@
-import type { NextFetchEvent, NextRequest } from 'next/server';
+import { NextResponse, type NextFetchEvent, type NextRequest } from 'next/server';
 import { nonApiMiddleware } from './middleware/authMiddleware';
 import { isAuthorized } from './middleware/isAuthorized';
+import next from 'next';
 
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
   const url = request.nextUrl;
+  
   if (!url.pathname.startsWith('/api')) {
     return nonApiMiddleware(request, event);
   }
+  // return isAuthorized(request);
   // return isAuthorized(request);
 }
 
