10bd356dac46ed54ef85510a30554de6de89b6be
Aditya Dadhich <adidadhich2003@gmail.com>
2025-02-04 12:24:55 +0530

    stashed

--- next.config.js
+++ next.config.js
@@ -1,49 +1,49 @@
-/** @type {import('next').NextConfig} */
- const nextConfig = {
-   images: {
-     domains: ['res.cloudinary.com'],
-   },
-   webpack(config) {
-     config.module.rules.push({
-       test: /\.mp3$/, // Match .mp3 files
-       type: 'asset/resource', // Handle as a resource
-       generator: {
-         filename: 'static/media/[name].[hash][ext]', // Output path for processed files
-       },
-     })
-     return config;
-   },
- }
- module.exports = nextConfig;
-
-
-
-
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//       domains: ['res.cloudinary.com'],
-//     },
-//   basePath: process.env.BASE_PATH,
-//   env: {
-//      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-     
-//   },
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.mp3$/, // Match .mp3 files
-//       type: 'asset/resource', // Handle as a resource
-//       generator: {
-//         filename: 'static/media/[name].[hash][ext]', // Output path for processed files
-//       },
-//     });
-
-//     return config;
-//   },
-//   reactStrictMode: false,
-// }
-
+//  const nextConfig = {
+//    images: {
+//      domains: ['res.cloudinary.com'],
+//    },
+//    webpack(config) {
+//      config.module.rules.push({
+//        test: /\.mp3$/, // Match .mp3 files
+//        type: 'asset/resource', // Handle as a resource
+//        generator: {
+//          filename: 'static/media/[name].[hash][ext]', // Output path for processed files
+//        },
+//      })
+//      return config;
+//    },
+//  }
 //  module.exports = nextConfig;
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      domains: ['res.cloudinary.com'],
+    },
+  basePath: process.env.BASE_PATH,
+  env: {
+     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+     
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.mp3$/, // Match .mp3 files
+      type: 'asset/resource', // Handle as a resource
+      generator: {
+        filename: 'static/media/[name].[hash][ext]', // Output path for processed files
+      },
+    });
+
+    return config;
+  },
+  reactStrictMode: false,
+}
+
+ module.exports = nextConfig;
 
 
 
